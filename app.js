d147fa2f2171c265a58ddc367384347029c88e0d
Jason Millhorn <jdmillhorn@gmail.com>
2023-08-14 15:41:05 -0600

    complete

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
 console.log('test')
 
-let home = 0
+let homeScore = 0
 
-let away = 0
+let awayScore = 0
+
+function updateDomHome() {
+    let homeElm = document.getElementById('homeScore')
+    // @ts-ignore
+    homeElm.innerHTML = homeScore
+}
+
+function updateDomAway() {
+    let awayElm = document.getElementById('awayScore')
+    // @ts-ignore
+    awayElm.innerHTML = awayScore
+}
+
+function onePointHome() {
+    homeScore += 1
+    updateDomHome()
+}
+
+function onePointAway() {
+    awayScore += 1
+    updateDomAway()
+}
+
+function threePointHome() {
+    homeScore += 3
+    updateDomHome()
+}
+
+function threePointAway() {
+    awayScore += 3
+    updateDomAway()
+}
+
+function startOver() {
+    homeScore = 0
+    awayScore = 0
+    updateDomHome()
+    updateDomAway()
+}
 
